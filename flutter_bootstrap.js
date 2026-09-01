54efce08b5ddad38f6d89339a56ecdd764311d41
spagnolocorrado-spec <spagnolocorrado@gmail.com>
2026-09-02 00:31:15 +0200

    Ottimizzato recupero GPS con timeout/fallback e corretto reset completo di stato e mappa

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1393202914" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3211221033" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
