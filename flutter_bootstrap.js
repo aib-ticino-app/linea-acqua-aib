b785e54c169064c977d4cbdd06152351c25ddbd7
spagnolocorrado-spec <spagnolocorrado@gmail.com>
2026-09-02 01:36:08 +0200

    Ricarica completa ripristina stato iniziale e accelerato centraggio GPS istantaneo

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5f77625673248ee5846fbcaf5d3e1a3878386f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3211221033" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "658393422" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
